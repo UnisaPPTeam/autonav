05adf146e3ff6053a6ea87a6140de728fc973468
PGG106 <72303893+PGG106@users.noreply.github.com>
2023-12-14 23:09:03 +0100

    align init pose formatting with the rest of the code

--- scripts/init_pose.py
+++ scripts/init_pose.py
@@ -3,27 +3,32 @@
 import rospy
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from nav_msgs.msg import Odometry
-from autonav.msg import cordinate
-#Node initialization
-rospy.init_node('init_pose')
-pub = rospy.Publisher('/initialpose', PoseWithCovarianceStamped, queue_size=1)
 
-# Construct message with the initial position
-init_msg = PoseWithCovarianceStamped()
-init_msg.header.frame_id = "map"
+def init_pose():
+    #Node initialization
+    rospy.init_node('init_pose')
+    pub = rospy.Publisher('/initialpose', PoseWithCovarianceStamped, queue_size=1)
 
-# Get initial pose from Gazebo
-odom_msg = rospy.wait_for_message('/odom', Odometry)
-init_msg.pose.pose.position.x = odom_msg.pose.pose.position.x
-init_msg.pose.pose.position.y = odom_msg.pose.pose.position.y
-init_msg.pose.pose.orientation.x = odom_msg.pose.pose.orientation.x
-init_msg.pose.pose.orientation.y = odom_msg.pose.pose.orientation.y
-init_msg.pose.pose.orientation.z = odom_msg.pose.pose.orientation.z
-init_msg.pose.pose.orientation.w = odom_msg.pose.pose.orientation.w
-rospy.sleep(1)
+    # Construct message with the initial position
+    init_msg = PoseWithCovarianceStamped()
+    init_msg.header.frame_id = "map"
 
-rospy.loginfo("setting initial info")
-pub.publish(init_msg)
-rospy.loginfo("initial pose set")
-initial_position = {'x':init_msg.pose.pose.position.x,'y':init_msg.pose.pose.position.y}
-rospy.set_param('initial_position',initial_position)
+    # Get initial pose from Gazebo
+    odom_msg = rospy.wait_for_message('/odom', Odometry)
+    init_msg.pose.pose.position.x = odom_msg.pose.pose.position.x
+    init_msg.pose.pose.position.y = odom_msg.pose.pose.position.y
+    init_msg.pose.pose.orientation.x = odom_msg.pose.pose.orientation.x
+    init_msg.pose.pose.orientation.y = odom_msg.pose.pose.orientation.y
+    init_msg.pose.pose.orientation.z = odom_msg.pose.pose.orientation.z
+    init_msg.pose.pose.orientation.w = odom_msg.pose.pose.orientation.w
+    rospy.sleep(1)
+
+    rospy.loginfo("setting initial info")
+    pub.publish(init_msg)
+    rospy.loginfo("initial pose set")
+    # Save initial position to the parameter server
+    initial_position = {'x':init_msg.pose.pose.position.x,'y':init_msg.pose.pose.position.y}
+    rospy.set_param('initial_position',initial_position)
+    
+if __name__ == "__main__":
+    init_pose()
