93195bafdf9049fcdc34faea2bf027f8b860b736
luigiPP93 <lavoroluigipp93@gmail.com>
2023-12-15 20:27:04 +0100

    dizionario

--- scripts/move.py
+++ scripts/move.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 
+import sys
 import rospy
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from autonav.srv import *
 from autonav.msg import cordinate
+
+# Definisci un dizionario per le coordinate delle stanze
+stanze_coordinates = {
+    "cucina": {"x": 4.5, "y": -2.55, "z": 0.0},
+    "salone": {"x": -4.45, "y": -0.45, "z": 0.0},
+    "stanza_da_letto": {"x": 3.25, "y": 1.3, "z": 0.0},
+    "bagno": {"x": -0.15, "y": 2.9, "z": 0.0},
+}
 
 x1 = 0.0
 y1 = 0.0
@@ -17,20 +26,19 @@ def get_distance(feedback):
         print("getting current node location")
         x2 = feedback.base_position.pose.position.x
         y2 = feedback.base_position.pose.position.y
-        resp1 = distance(x1, y1, x2 , y2)
+        resp1 = distance(x1, y1, x2, y2)
         return resp1.distance
     except rospy.ServiceException as e:
         print("Service call failed: %s" %e)
 
-#Callback definition
+# Callback definition
 def active_cb(extra):
     rospy.loginfo("Goal pose being processed")
 
 def feedback_cb(feedback):
-    #rospy.loginfo("Current location: " + str(feedback))
     print(f"the current robot distance from the starting point is {get_distance(feedback)}")
     return
-    
+
 def done_cb(status, result):
     if status == 3:
         rospy.loginfo("Goal reached")
@@ -39,8 +47,7 @@ def done_cb(status, result):
     if status == 4:
         rospy.loginfo("Goal aborted")
 
-
-def main():
+def navigate_to_room(room_name):
     rospy.init_node('goal_pose')
 
     navclient = actionlib.SimpleActionClient('move_base', MoveBaseAction)
@@ -50,22 +57,28 @@ def main():
     goal.target_pose.header.frame_id = "map"
     goal.target_pose.header.stamp = rospy.Time.now()
 
-    goal.target_pose.pose.position.x = 3.25
-    goal.target_pose.pose.position.y = 1.3
-    goal.target_pose.pose.position.z = 0.0
+    # Utilizza il dizionario per ottenere le coordinate della stanza specificata
+    room_coordinates = stanze_coordinates.get(room_name)
+    if room_coordinates:
+        goal.target_pose.pose.position.x = room_coordinates["x"]
+        goal.target_pose.pose.position.y = room_coordinates["y"]
+        goal.target_pose.pose.position.z = room_coordinates["z"]
+    else:
+        rospy.logerr(f"Room '{room_name}' not found in the coordinate dictionary.")
+        return
+
     goal.target_pose.pose.orientation.x = 0.0
     goal.target_pose.pose.orientation.y = 0.0
     goal.target_pose.pose.orientation.z = 0.0
     goal.target_pose.pose.orientation.w = 1
 
-
-    print("getting the robot initial position")
+    print(f"getting the robot initial position")
     data = rospy.get_param('initial_position')
     global x1, y1
     x1 = data['x']
     y1 = data['y']
   
-    print(f"Starting position set:{x1},{y1}")
+    print(f"Starting position set: {x1},{y1}")
     navclient.send_goal(goal, done_cb, active_cb, feedback_cb)
     finished = navclient.wait_for_result()
 
@@ -75,4 +88,9 @@ def main():
         rospy.loginfo(navclient.get_result())
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 2:
+        print("Usage: ./script_name.py <room_name>")
+        sys.exit(1)
+
+    room_name = sys.argv[1]
+    navigate_to_room(room_name)
